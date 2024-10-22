030f49aa27f7e1cdaeaad6277669a18a3c1a3ebe
OlivierSalin <98093066+OlivierSalin@users.noreply.github.com>
2024-10-22 11:31:48 +0200

    Update truth_tracking_telescope.py

--- Examples/Scripts/Python/truth_tracking_telescope.py
+++ Examples/Scripts/Python/truth_tracking_telescope.py
@@ -16,26 +16,16 @@ if "__main__" == __name__:
         stereos=[0] * 9,
     )
     
-    pos=[10000,  10500, 11000, 19500, 20000,20500]
-    n_layers = len(pos)
-    detector, trackingGeometry, decorators = acts.examples.TelescopeDetector.create(
-        bounds=[500, 1500], positions=pos, binValue=0,thickness=4,stereos=[0] * n_layers,
-    )
-    
     inputParticlePath = Path("")
     if not inputParticlePath.exists():
         inputParticlePath = None
 
 
-    # inputParticlePath = Path("/data/atlassmallfiles/users/salin/Acts_x/GEN/HepMC_Root/Root_DarkPhoton_X_e_mu/Particles_DarkPhoton_m0.6457_mu_mu.root")
-    # if not inputParticlePath.exists():
-    #     inputParticlePath = None
-
     outputDir = Path(f"./Output_ttk/")
     outputDir.mkdir(parents=True, exist_ok=True)
 
 
-    # field = acts.ConstantBField(acts.Vector3(0* u.T, 0, 1 * u.T))
+    #field = acts.ConstantBField(acts.Vector3(0* u.T, 0, 1 * u.T))
     field = acts.RestrictedBField(acts.Vector3(0* u.T, 0, 1.0 * u.T))
 
     srcdir = Path(__file__).resolve().parent.parent.parent.parent
